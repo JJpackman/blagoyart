de4b47b53193ec38c365a76138a88ce7d50ade0d
JJpackman <shyrokykh98@gmail.com>
2018-09-18 10:57:03 +0300

    Add header markup.

--- gulp/copy.js
+++ gulp/copy.js
@@ -1,14 +1,18 @@
-const gulp = require('gulp');
-const plugins = require('gulp-load-plugins')();
-const config = require('../config.json');
+const gulp = require("gulp");
+const plugins = require("gulp-load-plugins")();
+const config = require("../config.json");
 
-gulp.task('copy', function() {
-  return gulp.src([
-    config.src.img + '/**/*.{png,jpg}',
-    config.src.fonts + '/**/*.{woff,woff2}'
-  ], {
-    base: 'src'
-  })
+gulp.task("copy", function() {
+  return gulp
+    .src(
+      [
+        config.src.img + "/**/*.{png,jpg}",
+        config.src.fonts + "/**/*.{woff,woff2,ttf}"
+      ],
+      {
+        base: "src"
+      }
+    )
     .pipe(plugins.plumber())
     .pipe(gulp.dest(config.build.root));
 });
