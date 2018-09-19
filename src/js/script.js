211cbff6c72faa2e5cadcd5b447fe6cdaf3ad947
JJpackman <shyrokykh98@gmail.com>
2018-09-19 22:01:19 +0300

    Implement slider.

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+  $(".slider__slide-list").slick({
+    prevArrow: $(".slider__controls-btn--prev"),
+    nextArrow: $(".slider__controls-btn--next"),
+    dots: true
+  });
+});
