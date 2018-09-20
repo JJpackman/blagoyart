d9e4ab9f9987ee0d29f1c8ec6aa0385637918660
JJpackman <shyrokykh98@gmail.com>
2018-09-20 22:30:37 +0300

    Update 2018-09-20T19:30:03.253Z

--- gulp/deploy.js
+++
@@ -1,7 +0,0 @@
-const gulp = require("gulp");
-const plugins = require("gulp-load-plugins")();
-const config = require("../config.json");
-
-gulp.task("deploy", function() {
-  return gulp.src(config.build.root + "/**/*").pipe(plugins.ghPages());
-});
